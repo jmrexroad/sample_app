c5473e10d334df303e9a37a86706d7ef4ec88735
Justin Rexroad <jmrexroad@gmail.com>
2014-01-01 16:48:55 -0500

    make basic user model

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -30,6 +30,7 @@ describe 'Static pages' do
 	end
 
 	describe "Contact page" do
+		
 		before { visit contact_path }
 		it { should have_selector('h1', text: 'Contact') }
 		it { should have_selector('title', text: full_title('Contact'))}
